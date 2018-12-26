e6c088e513f94fb597bbeb3619d16c17a27a18e1
Idea_wowo <2571761894@qq.com>
2018-12-26 23:45:31 +0800

    添加数据库配置信息

--- src/test/java/com/platform/example/SoftPlatformApplicationTests.java
+++ src/test/java/com/platform/example/SoftPlatformApplicationTests.java
@@ -5,12 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SoftPlatformApplicationTests {
 
+//    @PersistenceContext
+//    EntityManager em;
+
     @Test
     public void contextLoads() {
+//        em.createQuery("select DISTINCT tid from t_loginuser");
     }
 
 }
